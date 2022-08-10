61d0322d05aa738d0d513aa89ea96da3cdf84ab5
shariy <sharii.margarita@gmail.com>
2022-08-11 00:52:23 +0300

    commit1

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,14 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first;
+        int second;
+        int third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
